3e8d74ce679c2d19558dbad2268ebc057e099a76
rstootalow <richardtutalo@gmail.com>
2017-09-04 20:51:47 -0600

    jQuery for navigation buttons complete

--- Resources/Javascript/script.js
+++ Resources/Javascript/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
    
+    /* For Sticky navigation */
     $('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -9,14 +10,55 @@ $(document).ready(function() {
     }, {
           offset: '60px;' /* This offset runs the above code 60px BEFORE the defined class .js--section-features*/
         });
+
     
+    /* Scroll on buttons */
+  
+    $('.js--scroll-to-plans').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
     
-    /*
-        var waypoints = $('#handler-first').waypoint(function(direction) {
-          notify(this.element.id + ' hit 25% from top of window') 
-        }, {
-          offset: '25%'
-        })
-    */
+    $('.js--scroll-to-start').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+    });
+    
+   /*NAVIGATION SCROLL*/
+    
+        $('a[href*="#"]')
+      // Remove links that don't actually link to anything
+            .not('[href="#"]')
+            .not('[href="#0"]')
+            .click(function(event) {
+        // On-page links
+            if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+            && 
+            location.hostname == this.hostname
+            ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000, function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              };
+            });
+          }
+        }
+      });
+    
     
 });
