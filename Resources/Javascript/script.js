d4d2ff758db879355e8e9170e2bbea626a913b19
rstootalow <richardtutalo@gmail.com>
2017-09-05 22:45:57 -0600

    jQuery animations for cities, iphone, and plans added

--- Resources/Javascript/script.js
+++ Resources/Javascript/script.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
     });
     
-   /*NAVIGATION SCROLL*/
+   /*NAVIGATION SCROLL USING SMOOTH SCROLLING ON CSS-TRICKS*/
     
         $('a[href*="#"]')
       // Remove links that don't actually link to anything
@@ -59,6 +59,38 @@ $(document).ready(function() {
           }
         }
       });
+    
+    /*ANIMATIONS ON SCROLL USING ANIMATE.CSS*/
+    
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+   
+    $('.js--wp-4').waypoint(function (direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+   
+    
+    
+    
+    
+    $
     
     
 });
