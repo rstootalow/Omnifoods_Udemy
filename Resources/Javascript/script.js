9146ba36ee6cecb4a5c53e023b5551c424c6051d
rstootalow <richardtutalo@gmail.com>
2017-09-08 18:55:23 -0600

    responsive nav for mobile complete

--- Resources/Javascript/script.js
+++ Resources/Javascript/script.js
@@ -87,10 +87,24 @@ $(document).ready(function() {
     });
    
     
+    /*MOBILE NAVIGATION*/
     
-    
-    
-    $
-    
+    $('.js--nav-icon').click(function() {
+       var nav = $('.js--main-nav');
+       var icon = $('.js--nav-icon i');
+        
+        
+        nav.slideToggle(200); /* Slide toggle opens and closes a box */
+        
+        
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+
+    });
     
 });
